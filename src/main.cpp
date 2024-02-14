360821347c681d7f6d4c6459e8fe06a6f330fa2d
Felix Meyer <felix.meyer@hs-osnabrueck.de>
2024-02-14 15:33:14 +0100

    Implemented controlled speedtest

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
 #include "Berkeley_Network.h"
 #include "Authentication_Server.h"
+#include "Serial_Master.h"
+
 #include "errors.h"
 #include "packets.h"
 
@@ -91,66 +93,112 @@ int main(int argc, char** argv) {
     AuthenticationServerImpl as( opts.resource_file.c_str(), opts.save_on_edit ); 
     Authenticator au(net, as);
 
+    SerialMaster serial_master("ttyUSB0");
+    UserDialog user_dialog;
+
     au.init();
 
-    uint8_t buffer[1534];
+    uint8_t buffer[1522];
     size_t n;
-    bool initial_frame = true;
     size_t received_bytes = 0;
 
-    std::chrono::high_resolution_clock::time_point start, end;
 
-    while(keepGoing) {
-        try {
+    auto speedtest = [&]() {
+        using namespace std::chrono;
+        bool speedtesting = true;
 
+        high_resolution_clock::time_point start, end;
+        PUF_Performance pp;
+
+        while(speedtesting) {
             n = net.receive(buffer, sizeof(buffer));
-            switch( deduce_type(buffer, n) ) {
-                case PUF_CON_E:
-                    if( au.accept(buffer, n) != 0) {
-                        std::cout << "Rejected" << std::endl;
-                    } else {
-                        std::cout << "Accepted" << std::endl;
+            if(deduce_type(buffer, sizeof(buffer)) != PUF_PERFORMANCE_E) {
+                continue;
+            }
+            
+            pp.from_binary(buffer, n);
+            switch( pp.get_data()[0] ) {
+                case 'F':
+                    start = high_resolution_clock::now();
+                case 'H':
+                    if( au.validate(pp, true) ) {
+                        received_bytes += pp.header_len();
                     }
                     break;
 
-
-                case PUF_PERFORMANCE_E:
-                    if(initial_frame) start = std::chrono::high_resolution_clock::now();
-
-                    PUF_Performance pp;
-                    pp.from_binary(buffer, n);
-
-                    if( !au.validate(pp, initial_frame) ) {
-                        std::cerr << "Error validating frame. Package is dropped." << std::endl;
-                    } else {
-                        received_bytes += sizeof(puf::PUF_Performance);
-                    }
-
-                    initial_frame = false;
-                    break;
-
-
-                case PUF_UNKNOWN_E:
-                    std::cout << "Unknown type" << std::endl;
+                case 'L':
+                    end = high_resolution_clock::now();
+                    received_bytes += pp.header_len();
+                    speedtesting = false;
                     break;
 
                 default:
+                    std::cerr << "Default statement reached\n";
                     ;
             }
-        } catch(const NetworkException& e) {
-        } catch(const Exception &e) {
-            puts(e.what());
+        }
+        auto duration = end-start;
+        double to_mbit_s = 8.0 / 1024;
+        std::cout << "Received for\t" << duration.count() << " ms" << std::endl;
+        std::cout << "Received\t" << received_bytes << " bytes" << std::endl;
+        std::cout << "Average speed:\t" << ((double)received_bytes/(double)duration.count()) * to_mbit_s << " mbit/s" << std::endl;
+    };
+
+
+    while(keepGoing) {
+        switch(user_dialog.get_user_input()) {
+            case STATUS:
+                serial_master.show_status();
+                break;
+
+            case CONFIG:
+                user_dialog.config_options();
+                break;
+
+            case CONNECT:
+                serial_master.slave_connect();
+                n = net.receive(buffer, sizeof(buffer));
+                if( deduce_type(buffer, sizeof(buffer)) != PUF_CON_E ) {
+                    break;
+                }
+
+                if(au.accept(buffer, n) != 0) {
+                    std::cout << "Rejected" << std::endl;
+                } else {
+                    std::cout << "Accepted" << std::endl;
+                }
+                break;
+
+            case UserInput::REGISTER:
+                serial_master.slave_sign_up();
+                au.sign_up();
+                break;
+
+            case SPEEDTEST:
+                serial_master.slave_speedtest();
+                speedtest();                
+                break;
+
+            case RECONNECT:
+                serial_master.reconnect();
+                break;
+
+            case ESP_STATUS:
+                serial_master.slave_status();
+                break;
+
+            case EXIT:
+                keepGoing = false;
+                break;
+
+            case UNKNOWN:
+                std::cout << "Unkown input" << std::endl;
+                break;
+
+            default:
+                ;
         }
     }
-
-    end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
-
-    double to_mbit_s = 8.0 / 1024;
-
-    std::cout << "Received for\t" << duration.count() << " ms" << std::endl;
-    std::cout << "Received\t" << received_bytes << " bytes" << std::endl;
-    std::cout << "Average speed:\t" << ((double)received_bytes/(double)duration.count()) * to_mbit_s << " mbit/s" << std::endl;
 
     } catch(const Exception &e) {
         puts(e.what());
